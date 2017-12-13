2c2d9f4c62093f49a626da642989579f723e37de
Jon Sellers <jsellers@gmail.com>
2017-12-13 15:16:15 -0500

    Final static todos

--- server.js
+++ server.js
@@ -27,16 +27,30 @@ var todos = [];
 // res.send("Todo API Root");
 // });
 
-// GET  /todos?completed=true
+// GET  /todos?completed=true&q=work
 app.get("/todos", function(req,res){
 		var queryParams = req.query;
 		var filteredTodos = todos;
+
 		if(queryParams.hasOwnProperty("completed") && queryParams.completed === "true"){
 		filteredTodos = _.where(filteredTodos, {completed: true });
 	}else if(queryParams.hasOwnProperty("completed") && queryParams.completed === "false"){
 filteredTodos = _.where(filteredTodos, {completed: false });
-
 	}
+
+if(queryParams.hasOwnProperty("q") && queryParams.q.trim().length >0){
+	filteredTodos= _.filter(filteredTodos, function(todo){
+		if(todo.description.toUpperCase().indexOf(queryParams.q.toUpperCase()) > -1){
+			return true;
+		}else{
+			return false;
+		}
+
+	});
+
+	
+}
+
 	res.json(filteredTodos);
 });
 
