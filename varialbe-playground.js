f283ab9c83fa44805c36b25e4dea83f33c8ac9f4
Jon Sellers <jsellers@gmail.com>
2017-12-13 13:19:29 -0500

    Add variable-playground area

---
+++ varialbe-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: "Andrew",
+// 	age: 21
+// }
+
+
+// function updatePerson(obj){
+// 	// obj = {
+// 	// 	name: "Andrew";
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+var grade = [15,37];
+
+function addGrade(newGrade){
+	grade.push(newGrade);
+	
+}
+
+addGrade(99);
+console.log(grade);
